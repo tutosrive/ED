54b16ca39cdf75906a6e5e51b8dc1b7dae8a133f
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2026-01-12 22:10:54 -0500

    Fix: Add basename to BrowserRouter to match Vite base URL

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ED">
       <App />
     </BrowserRouter>
   </StrictMode>,
